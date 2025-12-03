1c9dd2c42702ba7d8e77ec6c5731518671fbd136
Mahmoud El Hajj <86180377+Elytra2005@users.noreply.github.com>
2025-12-03 11:23:55 -0500

    Enhance logging with timestamp and UUID
    Refactored logging functionality to include timestamp and UUID. Added asynchronous file handling for logging messages.

--- LogExports.js
+++ LogExports.js
@@ -1,22 +1,26 @@
-// exporting 
-const path = require("path");
-const fileLocation = path.join(__dirname, "LogExports.js");
+const { format } = require('date-fns');
+const {v4: uuid} = require('uuid');
+
+// common core
+const fsPromises = require('fs').promises;
+const fs = require("fs");
+const path = require('path');
 
 
-const logModule = require(fileLocation);
-const eventEmitter = require("events");
 
-class Emitter extends eventEmitter {
-    
+const logMessage = async (message) => {
+    const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+    const LogItem = `${dateTime}\t ${uuid()}\t${message}\n`;
+    console.log(LogItem);
+
+    try {
+      if(!fs.existsSync(path.join(__dirname, 'Logs'))) {
+        await fsPromises.mkdir(path.join(__dirname, 'Logs'));
+      }
+      await fsPromises.appendFile(path.join(__dirname, "Logs", "EventLogs.txt"), LogItem);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-const myemitter = new Emitter();
-
-myemitter.on('log', (msg) => {
-    logModule(msg);
-});
-
-setTimeout(() => {
-    myemitter.emit('log', 'hello guys')
-}, 3000);
-
+module.exports = logMessage;
